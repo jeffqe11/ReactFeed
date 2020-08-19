f43d6f46e7fab75428d42b9be0f8e8e6bf8763d5
Quesada Jefferson (SO/OPM1.4-Hda) <jefferson.quesada@bosch.com>
2020-08-19 15:46:37 -0600

    Updates

---
+++ precache-manifest.f98b71f21945f571e9433f01606410b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dccb3215161a8d75a261e19239670a",
+    "url": "/ReactFeed/index.html"
+  },
+  {
+    "revision": "09ee18a8c1c268e8c904",
+    "url": "/ReactFeed/static/css/main.977f1fdd.chunk.css"
+  },
+  {
+    "revision": "b86dc95f4dc3d43123c2",
+    "url": "/ReactFeed/static/js/2.ad19a609.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/ReactFeed/static/js/2.ad19a609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ee18a8c1c268e8c904",
+    "url": "/ReactFeed/static/js/main.490491f9.chunk.js"
+  },
+  {
+    "revision": "d8a40b7e6f408d0e67ad",
+    "url": "/ReactFeed/static/js/runtime-main.c0ba9123.js"
+  }
+]);
